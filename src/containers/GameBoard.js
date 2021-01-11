73af3284f09a7f1d2e1975968863a60117a8155b
Eli Cusic <tntitans31@gmail.com>
2021-01-11 14:17:19 -0600

    set up buttons to be generated within table from function

--- src/containers/GameBoard.js
+++ src/containers/GameBoard.js
@@ -7,10 +7,9 @@ export class GameBoard extends Component {
     generateCells = () => {
         let cells = []
         for (let i = 1; i <= 7; i++){
-            let column = "col-" + i.toString()
+            let column = `col-${i}`
             cells.push(
                 <td className={column}>
-                    {console.log(i)}
                     <Cell />
                 </td>
             )
@@ -23,12 +22,28 @@ export class GameBoard extends Component {
         for (let i = 1; i <= 6; i++){
             rows.push(
                 <tr className="row">
-                    {console.log(i)}
                     { this.generateCells() }
                 </tr>
             )
         }
         return rows.map(row => row)
+    }
+
+    generateButtons = () => {
+        let buttons = []
+        for (let i = 1; i <= 7; i++){
+            let id = `token-button-${i}`
+            buttons.push(
+                <td>
+                    <ButtonCell />
+                </td>
+            )
+        }
+        return (
+            <tr id="button-row">
+                { buttons.map(button => button) }
+            </tr>
+        )
     }
 
     render() {
@@ -37,15 +52,7 @@ export class GameBoard extends Component {
                 <table id="game-board-table">
                     <tbody>
                         { this.generateRows() }
-                        <tr id="button-row">
-                            <td id="token-button-1"><ButtonCell /></td>
-                            <td id="token-button-2"><ButtonCell /></td>
-                            <td id="token-button-3"><ButtonCell /></td>
-                            <td id="token-button-4"><ButtonCell /></td>
-                            <td id="token-button-5"><ButtonCell /></td>
-                            <td id="token-button-6"><ButtonCell /></td>
-                            <td id="token-button-7"><ButtonCell /></td>
-                        </tr>
+                        { this.generateButtons() }
                     </tbody>
                 </table>
             </div>
