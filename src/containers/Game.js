94b5cbb5ea6719bc4849433eb31b5713d172346e
Eli Cusic <tntitans31@gmail.com>
2021-01-12 07:43:32 -0600

    set up constructor in Game with state that initializes with nested array of Tokens

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 import GameBoard from './GameBoard'
 import CardPanel from './CardPanel'
+import Token from '../components/Token'
 
 export class Game extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      tokens: this.initializeTokens()
+    }
+  }
+
+  initializeTokens() {
+    let nestedArray = []
+    for (let i = 0; i < 6; i++) {
+      let rowArray = []
+      for (let j = 0; j < 7; j++) {
+        rowArray.push(<Token coordinates={[j, i]} color={"F5F5F5"}/>)
+      }
+      nestedArray.push(rowArray)  
+    }
+    console.log(nestedArray)
+    return nestedArray
+  }
+
   render() {
+    {console.log(this.state)}
     return (
       <div>
         <div id="card-panel">
