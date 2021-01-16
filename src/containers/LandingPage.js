03f7597629f3b426a8d48a34dd3ecc35e4dd130b
Eli Cusic <tntitans31@gmail.com>
2021-01-15 19:28:40 -0600

    set up empty buttons to practice triggering fetch actions

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -5,9 +5,47 @@ export class LandingPage extends Component {
     return (
       <div>
         Landing Page Component
+        <div className="trial-buttons">
+            <GetMuppetsButton />
+            <GetCardsButton />
+            <GetGameLogsButton />
+            <SendGameLogButton />
+        </div>
       </div>
     )
   }
+}
+
+const GetMuppetsButton = () => {
+  return (
+    <div>
+      <button>GetMuppetsButton</button>
+    </div>
+  )
+}
+
+const GetCardsButton = () => {
+  return (
+    <div>
+        <button>GetCardsButton</button>
+    </div>
+)
+}
+
+const GetGameLogsButton = () => {
+  return (
+    <div>
+        <button>GetGameLogsButton</button>
+    </div>
+  )
+}
+
+const SendGameLogButton = () => {
+  return (
+    <div>
+      <button>SendGameLogButton</button>
+    </div>
+  )
 }
 
 export default LandingPage
