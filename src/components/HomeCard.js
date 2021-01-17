c29e806569a494568791e8ae26d5d2287dad0988
Eli Cusic <tntitans31@gmail.com>
2021-01-17 10:54:28 -0600

    set up home card

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -3,7 +3,14 @@ import React from 'react'
 const HomeCard = () => {
     return (
       <div id="home-card">
-        Home Card goes here
+        <div class="card">
+            <div class="card-image">
+                <img src="https://images.immediate.co.uk/production/volatile/sites/3/2020/05/Muppets-Now-0c065ce.jpg?quality=90&crop=665px%2C98px%2C1247px%2C831px&resize=620%2C413" />
+            </div>
+            <div class="card-content center-align dark-green">
+                <div>THIS IS THE HOME PAGE</div>
+            </div>
+        </div>
       </div>
     )
 }
